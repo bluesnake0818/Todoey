88d038a091138e8bb6a2e6e92fbd1191ce6f8694
bluesnake0818 <raidient87@gmail.com>
2024-06-23 16:58:13 -0400

    create CategoryViewController and Data Model, Model Relationship with Item Model

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by William Baek on 6/23/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    var categories = [Category]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - TableView Datasource Methods
+       
+   // MARK: - Data Manipulation Methods
+       
+    // MARK: - Add New Categories
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+   
+    
+    // MARK: - TableView Delegate Methods
+    
+}
